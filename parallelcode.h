f57cc2e2fbe02ce20ff1e986eb58399f608a5529
Antonio <147176897+TonIann7@users.noreply.github.com>
2024-06-05 10:02:13 +0200

    Update parallelcode.h

--- parallelcode.h
+++ parallelcode.h
@@ -1,6 +1,14 @@
 #ifndef PARALLELCODE_H
 #define PARALLELCODE_H 
 
+void sum_vectors(float *a, float *b, float *c, int N);
+
+double *sommavettori2(int N,double *a,double *b,int th);
+
+float sum_numbers_first(float *a,int N);
+
+float sum_numbers_second(float *a,int N,int th);
+
 int** createallocateMatrix(int rows, int columns);
 
 int* createArray(int n);
@@ -10,12 +18,6 @@ int* createallocateArray(int n);
 void printMatrix(int** matrix, int rows, int columns);
 
 void printArray(int a[], int n);
-
-void sum_vectors(float *a, float *b, float *c, int N);
-
-float sum_numbers_first(float *a,int N);
-
-float sum_numbers_second(float *a,int N);
 
 double pigreco1(int N);
 
@@ -29,13 +31,20 @@ int parallelsearch(int f,int l,int token,int n,int *a);
 
 void oddEvenSort(int *a, int N);
 
-void prodottoscalarematrice1(float **A, float B,int n,int m);
+void prodottoscalarematrice1(float **A, float B,int righe,int colonne,int th);
 
-void prodottoscalarematrice2(float **A, float B,int n,int m);
+void prodottoscalarematrice2(float **A, float B,int righe,int colonne, int th);
 
-void prodottoscalarematrice3(float **A, float alpha,int n,int m,int blockSize);
+void prodottoscalarematrice3(float **A, float alpha,int n,int m,int blockSize,int th);
 
-double * matxvet_righe(int m, int n, double *x, double **A);
+double * matxvet_righe(int m, int n, double *x, double **A, int th);
 
+double * matxvet_colonne(int righe, int colonne, double *x, double **A,int th);
+
+double * matxvet_colonne2(int righe, int colonne, double *x, double **A,int th);
+
+double * matxvet_colonne3(int righe, int colonne, double *x, double **A,int th);
+
+double * matxvet_terzastrat(int righe,int colonne,int blocksize, double *x,double **A,int th);
 
 #endif
